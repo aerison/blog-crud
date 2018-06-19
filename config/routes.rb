a46591c6b05a4dea85a75c5e7d54cdedebd97322
aerison <sonsoo1224@gmail.com>
2018-06-19 05:36:35 +0000

    crud board1

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'post/new'
 
   get 'post/create'
+  get 'post/show'
+  get 'post/:id'=>'post#show'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
