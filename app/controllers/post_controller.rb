e140d38f1638e4fb4a1c882360fa89fd484cb82a
aerison <sonsoo1224@gmail.com>
2018-06-19 05:24:54 +0000

    crud

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,10 +1,20 @@
 class PostController < ApplicationController
   def index
+    @posts=Post.all
   end
 
   def new
   end
 
   def create
+    #1.
+    Post.create(title:params[:title],body:params[:body])
+    #1-1.
+    # Post.create(:title=>params[:title], :body=>params[:body])
+    #2.
+    # post=Post.new
+    # post.title=params[:title]
+    # post.body=params[:body]
+    # post.save
   end
 end
